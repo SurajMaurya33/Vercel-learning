ecc333033b6c85a300679f06e8a41c48d1df915f
SurajMaurya33 <114276787+SurajMaurya33@users.noreply.github.com>
2024-08-05 17:18:27 +0530

    Add files via upload

--- src/components/Shared/_Layout.jsx
+++ src/components/Shared/_Layout.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import Header from './Header'
+import Aside from './Aside'
 
 const _Layout = () => {
   return (
     <div>
+      <Header/>
+      
       <nav>
         <ul>
           <li>
